5500436e069869273e3ff4c09191429339b0f671
NastiaKoval <anasteziyam@gmail.com>
2020-03-19 23:51:42 +0200

    feat: make 9-10 tasks

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,25 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+    str += '';
+
+    if ('addition' in options) {
+        let addition = new Array(options.additionRepeatTimes).fill(String(options.addition));
+        if ('additionSeparator' in options) {
+            addition = addition.join(options.additionSeparator);
+        } else {
+            addition = addition.join('|');
+        }
+        str += addition;
+    }
+
+
+    
+    let result = new Array(options.repeatTimes).fill(str);
+    
+    if ('separator' in options) {
+        return result.join(options.separator);
+    } else {
+        return result.join('+');
+    }
+    
 };
   
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,12 +1,68 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    constructor(modification) {
+        this.modification = modification;
     }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    encrypt(message, key) {
+        message = message.toUpperCase();
+        key = key.toUpperCase();
+        
+        let result = [];
+        let num;
+        let j = 0;
+
+        for (let i = 0; i < message.length; i++) {
+            if (message.codePointAt(i) < 65 || message.codePointAt(i) > 90) {
+                result.push(message[i]);
+            } else {
+                if (j < key.length) {
+                    num = (message.codePointAt(i) + key.codePointAt(j)) % 26 + 65;
+                    result.push(String.fromCodePoint(num));
+                    j++;
+                } else {
+                    num = (message.codePointAt(i) + key.codePointAt(j-key.length*(Math.floor(j / key.length)))) % 26 + 65;
+                    result.push(String.fromCodePoint(num));
+                    j++;
+                } 
+            }
+        }
+
+        if (this.modification === false) {
+            return result.reverse().join('');
+        } else {
+            return result.join('');
+        }
+    }
+
+    decrypt(encryptedMessage, key) {
+        key = key.toUpperCase();
+
+        let result = [];
+        let num;
+        let j = 0;
+
+        for (let i = 0; i < encryptedMessage.length; i++) {
+            if (encryptedMessage.codePointAt(i) < 65 || encryptedMessage.codePointAt(i) > 90) {
+                result.push(encryptedMessage[i]);
+            } else {
+                if (j < key.length) {
+                    num = (encryptedMessage.codePointAt(i) + 26 - key.codePointAt(j)) % 26 + 65;
+                    result.push(String.fromCodePoint(num));
+                    j++;
+                } else {
+                    num = (encryptedMessage.codePointAt(i) +26 - key.codePointAt(j-key.length*(Math.floor(j / key.length)))) % 26 + 65;
+                    result.push(String.fromCodePoint(num));
+                    j++;
+                } 
+            }
+            
+        }
+
+        if (this.modification === false) {
+            return result.reverse().join('');
+        } else {
+            return result.join('');
+        }
     }
 }
 
